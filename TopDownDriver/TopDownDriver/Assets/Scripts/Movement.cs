e3ef779b2421bc79beb26651ed110ab327365c4e
cobe <cobibitza@gmail.com>
2018-09-18 15:01:29 +0200

    first commit

---
+++ TopDownDriver/TopDownDriver/Assets/Scripts/Movement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float xspeep = 0f;
+	public float power = 0.001f;
+	public float friction = 0.95f;
+	public bool right = false;
+	public bool left = false;
+	public float speed;
+	public Rigidbody body;
+
+	void Start(){
+		body = GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void FixedUpdate () {
+
+
+		if(right){
+			xspeep += power;
+		}
+		if(left){
+			xspeep -= power;
+		}
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0)){
+			if (Input.mousePosition.x > 390f) {
+				right = true;
+			} else {
+				left = true;
+			}
+		}
+		if(Input.GetMouseButtonUp(0)){
+			if (Input.mousePosition.x > 390f) {
+				right = false;
+			} else {
+				left = false;
+			}
+		}
+
+		xspeep *= friction;
+		body.velocity = Vector3.right * speed;
+		//Ovo radi dobro
+//		transform.Translate (-Vector3.forward * speed);
+		transform.Translate(Vector3.right * -xspeep);
+
+
+
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Road") {
+			Debug.Log (other.name);
+			RoadMaker.instance.PlaceNew ();
+		}
+	}
+}
---
+++ TopDownDriver/TopDownDriver/Assets/Scripts/RoadMaker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadMaker : MonoBehaviour {
+
+	public float lastRoadXPosition;
+	public List<Transform> roadParts;
+	public float time;
+	public static RoadMaker instance;
+
+
+	void Awake(){
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 60;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null) {
+			instance = this;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlaceNew(){
+		Transform oldPart = roadParts [0];
+		roadParts.RemoveAt (0);
+		oldPart.localPosition = new Vector3 (lastRoadXPosition + 40, oldPart.localPosition.y, oldPart.localPosition.z);
+		roadParts.Add (oldPart);
+		lastRoadXPosition += 40;
+	}
+}
